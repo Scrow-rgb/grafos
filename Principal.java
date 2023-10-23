5e3ca52399a32a00207b6efc49544ef217208ed5
scrow-rgb <macrinho32@gmail.com>
2023-10-23 17:05:31 -0300

    refactor: update

--- Principal.java
+++ Principal.java
@@ -1,21 +1,33 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Grafo grafo = new Grafo(5, false, false);
+        Grafo grafo = new Grafo(5, true, false);
+        Scanner leitor = new Scanner(System.in);
 
         grafo.inserirAresta(0, 1, 1);
         grafo.inserirAresta(0, 2, 1);
         grafo.inserirAresta(1, 3, 1);
+        grafo.inserirAresta(1, 4, 1);
         grafo.inserirAresta(2, 3, 1);
-        grafo.inserirAresta(2, 4, 1);
-        grafo.inserirAresta(3, 4, 1);
+        grafo.inserirAresta(2, 1, 1);
 
-        System.out.println("Grau do vértice 2: " + grafo.consultarGrauVertice(2));
+        int vertice;
+
+        System.out.println("Informe o vertice a ser consultado");
+        vertice = leitor.nextInt();
+
+        System.out.println("Grau do vértice " + vertice + ": " + grafo.consultarGrauVertice(vertice));
         System.out.println("Grau do grafo: " + grafo.consultarGrauGrafo());
 
-        List<Aresta> vizinhos = grafo.consultarVizinhos(2);
-        System.out.print("Vizinhos do vértice 2: ");
+        int vizinho;
+
+        System.out.println("Informe qual vertice voce quer saber o vizinho");
+        vizinho = leitor.nextInt();
+
+        List<Aresta> vizinhos = grafo.consultarVizinhos(vizinho);
+        System.out.print("Vizinhos do vértice " + vizinho + ": ");
         for (Aresta aresta : vizinhos) {
             System.out.print(aresta.getDestino() + " ");
         }
@@ -30,8 +42,17 @@ public class Principal {
         boolean completo = grafo.verificarCompleto();
         System.out.println("O grafo é completo? " + completo);
 
-        System.out.println("Busca em Largura a partir do vértice 0:");
-        grafo.buscaEmLargura(0);
+
+        int origem;
+
+        System.out.println("Qual vertice quer comecar para fazer a busca?");
+        origem = leitor.nextInt();
+
+        System.out.println("Busca em Largura a partir do vértice " + origem + ": ");
+        grafo.buscaEmLargura(origem);
         System.out.println();
+
+
+        leitor.close();
     }
 }
